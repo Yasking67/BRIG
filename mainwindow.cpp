6f1078896249541f23e216a587ba16367367fd09
Vernigor Nikita <nikita.vernigor@gmail.com>
2022-06-14 17:39:58 +0300

    added develop

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
-//тестовая запись для проверки гитхаб
+//тестовая запись для проверки гитхаб develop
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
